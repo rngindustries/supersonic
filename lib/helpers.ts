3f690c304e1c1dd7aab4b541776bd088f5744cb8
raza <101937798+razahai@users.noreply.github.com>
2024-08-08 01:36:31 -0400

    chore(helpers): move enums to top
    *for organization

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,17 +1,5 @@
 import { glob as _glob, GlobOptions } from "glob";
 
-export async function glob(pattern: string | string[], options?: GlobOptions) {
-    // allows windows paths without removing the ability to escape glob patterns (see: windowsPathsNoEscape)
-    if (typeof pattern === "string") 
-        pattern = pattern.replaceAll(/\\/g, "/");
-    else
-        pattern = pattern.map(pat => pat.replaceAll(/\\/g, "/"))
-
-    if (options)
-        return await _glob(pattern, options);    
-    return await _glob(pattern);
-}
-
 export enum OptionType {
     "str" = 3,
     "int" = 4,
@@ -22,4 +10,16 @@ export enum OptionType {
     "ment" = 9,
     "num" = 10,
     "att" = 11
+}
+
+export async function glob(pattern: string | string[], options?: GlobOptions) {
+    // allows windows paths without removing the ability to escape glob patterns (see: windowsPathsNoEscape)
+    if (typeof pattern === "string") 
+        pattern = pattern.replaceAll(/\\/g, "/");
+    else
+        pattern = pattern.map(pat => pat.replaceAll(/\\/g, "/"))
+
+    if (options)
+        return await _glob(pattern, options);    
+    return await _glob(pattern);
 }
