2542271f2bbb3274c490730aaf4822ffc904a08e
raza <101937798+razahai@users.noreply.github.com>
2024-08-11 21:15:14 -0400

    fix(component): interaction must be correct type for all components

--- lib/handlers/component.ts
+++ lib/handlers/component.ts
@@ -1,7 +1,7 @@
-import { ButtonInteraction } from "discord.js";
+import { ButtonInteraction, MessageComponentInteraction } from "discord.js";
 import { Component } from "../types";
 
-export function component(name: string, callback: (interaction: ButtonInteraction) => void) {
+export function component(name: string, callback: (interaction: MessageComponentInteraction) => void) {
     return {
         name: name,
         execute: callback
