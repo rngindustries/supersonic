60158197ec5f5bd9a8f4c73b6e1d589c1dac1176
raza <101937798+razahai@users.noreply.github.com>
2025-07-23 21:23:17 -0400

    add ability to attach component modules manually

--- lib/handlers/component.ts
+++ lib/handlers/component.ts
@@ -1,16 +1,34 @@
 import { ButtonInteraction, MessageComponentInteraction } from "discord.js";
 import { Component, Supersonic } from "../types";
 
-export function component<T extends MessageComponentInteraction>(name: string, callback: (interaction: T) => void) {
+export function component<T extends MessageComponentInteraction>(
+    name: string, 
+    callback: (interaction: T) => void
+): Component {
     return {
         name: name,
         execute: callback
     } as Component;
 }
 
-export function click(this: Supersonic, name: string, callback: (interaction: ButtonInteraction) => void) {
-    this.components.button.set(name, {
-        name: name,
-        execute: callback
-    } as Component);
+// TODO: add more components (e.g., select menus)
+export function click(
+    this: Supersonic,
+    component: Component
+): void;
+export function click(
+    this: Supersonic,
+    name: string,
+    callback: (interaction: ButtonInteraction) => void
+): void;
+export function click(
+    this: Supersonic, 
+    component: string | Component, 
+    callback?: (interaction: ButtonInteraction) => void
+): void {
+    let componentModule = typeof component === "string"
+        ? { name: component, execute: callback! } as Component
+        : component;
+
+    this.components.button.set(componentModule.name, componentModule);
 }
